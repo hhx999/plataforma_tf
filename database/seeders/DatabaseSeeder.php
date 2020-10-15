6c414332d6ad005c8deb8a639e403aa9d25fb3f7
Borjas Vega Riveras <borjas.vegariveras@gmail.com>
2020-10-15 01:12:42 -0300

    TG-2 #closed Agregados los seeders de tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(CategoriesTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
         $this->call(PostsTableSeeder::class);
     }
 }
