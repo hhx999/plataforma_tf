7bec111ff6988bf2186f5d85a83cbd61e7c0b48d
Borjas Vega Riveras <borjas.vegariveras@gmail.com>
2020-10-15 18:53:48 -0300

    TG-31 #closed Configurando platilla adminlte en admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layout')
+
+@section('content')
+
+<h1>Contenido de página</h1>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
     return view('welcome' ,compact('posts'));
 });
 
-Route::get('posts', function() {
-	return App\Models\Post::all();
+Route::get('admin', function() {
+	return view('admin.dashboard');
 });
